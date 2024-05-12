6860601275d0f91ae757511537dd598b9389145f
Sagi Carmel <ace.code.pt@gmail.com>
2024-05-12 21:50:37 +0300

    feat: escape now works only on toggle term

--- .config/nvim/lua/plugins/terminal.lua
+++ .config/nvim/lua/plugins/terminal.lua
@@ -4,12 +4,13 @@ return {
 		version = "*",
 		config = function()
 			local util = require("util.map")
-			require("toggleterm").setup({
-				start_in_insert = true,
-			})
+			require("toggleterm").setup()
 			function _G.set_terminal_keymaps()
 				local opts = { buffer = 0, noremap = true }
-				util.map("t", "<C-t>", [[<Cmd>ToggleTerm<CR>]], opts)
+				util.map("t", "<ESC>", [[<C-\><C-n>]], opts)
+				util.map("t", "<C-t>", function()
+					vim.cmd([[ToggleTerm]])
+				end, opts)
 				util.map("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
 				util.map("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
 				util.map("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
@@ -18,11 +19,16 @@ return {
 			end
 
 			-- Set mapping
-			vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
-			-- Start by inserting by default
-			vim.cmd("autocmd! BufWinEnter,WinEnter term://* startinsert")
+			vim.cmd("autocmd! TermOpen term://*toggleterm* lua set_terminal_keymaps()")
 
-			util.map("n", "<C-t>", [[<Cmd> ToggleTerm<CR>]], { silent = false })
+			-- Start by inserting by default
+			vim.api.nvim_create_autocmd({ "TermOpen", "BufEnter" }, {
+				pattern = { "term://*toggleterm*" },
+				callback = function()
+					vim.cmd(":startinsert")
+				end,
+			})
+			util.map("n", "<C-t>", [[<Cmd> ToggleTerm<CR>]])
 		end,
 	},
 }

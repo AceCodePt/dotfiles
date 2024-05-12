606a969c30ea525a76cf10a2c8b0be7d1b757fe7
Sagi Carmel <ace.code.pt@gmail.com>
2024-05-12 17:41:06 +0300

    feat: created a new keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -56,5 +56,8 @@ map("v", ">", ">gv")
 map("v", "K", ":move '>+1<CR>gv-gv")
 map("v", "J", ":move '<-2<CR>gv-gv")
 
+map({"n", "v"}, "<leader>q","<CMD>wa<CR><CMD>qa<CR>")
+
 -- Trick from the primagen
 map("n", "<leader>v", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]])
+

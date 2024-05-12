0a29a0c8b44268b7ecdefc9e2d2d38a67a5bb6b8
Sagi Carmel <ace.code.pt@gmail.com>
2024-05-12 17:41:43 +0300

    feat: changed to git find files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,7 +6,7 @@ return {
       init = function()
          local builtin = require("telescope.builtin")
 
-         vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+         vim.keymap.set('n', '<leader>sf', builtin.git_files, { desc = '[S]earch [F]iles' })
          vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
          vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
 

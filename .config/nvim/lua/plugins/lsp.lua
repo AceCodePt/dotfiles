d3581ac0bd627183e295da3cb6c4217389e7365e
Sagi Carmel <ace.code.pt@gmail.com>
2024-05-15 08:27:43 +0300

    chore: changed lsp language

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,93 +2,87 @@
 --  This function gets run when an LSP connects to a particular buffer.
 
 local languages = {
-   "clangd",
-   "astro",
-   "html",
-   "cssls",
-   "tsserver",
-   "tailwindcss",
-   "emmet_ls",
-   "pyright",
-   "html"
+	"lua_ls",
+	"clangd",
+	"html",
+	"cssls",
+	"tsserver",
+	"eslint",
+	"tailwindcss",
+	"pyright",
+	"gopls",
 }
 
 return {
-   {
-      "neovim/nvim-lspconfig",
-      dependencies = { "hrsh7th/cmp-nvim-lsp" },
-      config = function()
-         local capabilities = require("cmp_nvim_lsp").default_capabilities()
-         require("lspconfig").lua_ls.setup({
-            capabilities = capabilities,
-            settings = {
-               Lua = {
-                  diagnostics = {
-                     globals = { "vim", "describe", "it" },
-                  },
-               },
-            },
-         })
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "hrsh7th/cmp-nvim-lsp" },
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			require("lspconfig").lua_ls.setup({
+				on_init = function(client)
+					local path = client.workspace_folders[1].name
+					if
+						not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc")
+					then
+						client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+							runtime = {
+								-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+								version = "LuaJIT",
+							},
+							workspace = {
+								library = { vim.env.VIMRUNTIME },
+							},
+						})
 
-         local on_attach = function(_, bufnr)
-           -- In this case, we create a function that lets us more easily define mappings specific
-           -- for LSP related items. It sets the mode, buffer and description for us each time.
-           local nmap = function(keys, func, desc)
-             if desc then
-               desc = 'LSP: ' .. desc
-             end
+						client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+					end
+					return true
+				end,
+			})
 
-             vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-           end
+			local on_attach = function(_, bufnr)
+				-- In this case, we create a function that lets us more easily define mappings specific
+				-- for LSP related items. It sets the mode, buffer and description for us each time.
+				local nmap = function(keys, func, desc)
+					if desc then
+						desc = "LSP: " .. desc
+					end
 
-           nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-           nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-         end
+					vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+				end
 
-         require("lspconfig").tailwindcss.setup({
-            on_attach = function()
-               require("tailwindcss-colors").buf_attach(0)
-            end,
-         })
+				nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+				nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+			end
 
-         for _, language in pairs(languages) do
-            require("lspconfig")[language].setup({
-               capabilities = capabilities,
-               on_attach = on_attach
-            })
-         end
+			require("lspconfig").tailwindcss.setup({
+				on_attach = function()
+					require("tailwindcss-colors").buf_attach(0)
+				end,
+			})
 
-         vim.keymap.set(
-            "n",
-            "<Leader>fa",
-            ":EslintFixAll<CR>",
-            { noremap = true, silent = true }
-         )
+			for _, language in pairs(languages) do
+				require("lspconfig")[language].setup({
+					capabilities = capabilities,
+					on_attach = on_attach,
+				})
+			end
 
-         vim.lsp.handlers["textDocument/publishDiagnostics"] =
-             vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-                virtual_text = false,
-             })
-      end,
-   },
-   {
-      "williamboman/mason.nvim",
-      opts = {},
-   },
-   {
-      "williamboman/mason-lspconfig.nvim",
-      opts = {
-         ensure_installed = {
-            "lua_ls",
-            "clangd",
-            "html",
-            "cssls",
-            "tsserver",
-            "eslint",
-            "tailwindcss",
-            "pyright",
-            "gopls",
-         },
-      },
-   },
+			vim.lsp.handlers["textDocument/publishDiagnostics"] =
+				vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+					virtual_text = false,
+				})
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = languages,
+		},
+	},
 }

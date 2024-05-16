5cf2ceef29bf8895070257351436e13ef30b1c57
Sagi Carmel <ace.code.pt@gmail.com>
2024-05-16 09:15:57 +0300

    feat: removed lspkind and used renewed the comp sort

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -9,19 +9,31 @@ return {
 			"hrsh7th/cmp-buffer",
 			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
-			"onsails/lspkind.nvim",
 		},
 		config = function()
-			local ls = require("luasnip")
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			local types = require("cmp.types")
+			local compare = require("cmp.config.compare")
+
 			vim.keymap.set({ "i", "s" }, "<C-L>", function()
-				ls.jump(1)
+				luasnip.jump(1)
 			end, { silent = true })
 			vim.keymap.set({ "i", "s" }, "<C-J>", function()
-				ls.jump(-1)
+				luasnip.jump(-1)
 			end, { silent = true })
 
-			local lspkind = require("lspkind")
-			local cmp = require("cmp")
+			local modified_priority = {
+				[types.lsp.CompletionItemKind.Variable] = types.lsp.CompletionItemKind.Method,
+				[types.lsp.CompletionItemKind.Snippet] = 0, -- top
+				[types.lsp.CompletionItemKind.Keyword] = 1, -- top
+				[types.lsp.CompletionItemKind.Text] = 100, -- bottom
+			}
+
+			local function modified_kind(kind)
+				return modified_priority[kind] or kind
+			end
+
 			cmp.setup({
 				snippet = {
 					expand = function(args)
@@ -57,18 +69,43 @@ return {
 					end
 				end,
 				formatting = {
-					format = lspkind.cmp_format({
-						mode = "symbol_text",
-						maxwidth = 50,
-						ellipsis_char = "...",
-						menu = {
-							buffer = "[Buffer]",
-							nvim_lsp = "[LSP]",
-							nvim_lua = "[Lua]",
-							luasnip = "[LuaSnip]",
-							latex_symbols = "[Latex]",
-						},
-					}),
+					format = function(_, vim_item)
+						local kind_icons = vim.g.personal_options.lsp_icons
+						vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind)
+						-- I don't want to see any menu items
+						vim_item.menu = ""
+						return vim_item
+					end,
+				},
+				sorting = {
+					-- https://github.com/hrsh7th/nvim-cmp/blob/main/lua/cmp/config/compare.lua
+					comparators = {
+						compare.offset,
+						compare.exact,
+						function(entry1, entry2) -- sort by length ignoring "=~"
+							local len1 = string.len(string.gsub(entry1.completion_item.label, "[=~()_]", ""))
+							local len2 = string.len(string.gsub(entry2.completion_item.label, "[=~()_]", ""))
+							if len1 ~= len2 then
+								return len1 - len2 < 0
+							end
+						end,
+						compare.recently_used,
+						function(entry1, entry2) -- sort by compare kind (Variable, Function etc)
+							local kind1 = modified_kind(entry1:get_kind())
+							local kind2 = modified_kind(entry2:get_kind())
+							if kind1 ~= kind2 then
+								return kind1 - kind2 < 0
+							end
+						end,
+						function(entry1, entry2) -- score by lsp, if available
+							local t1 = entry1.completion_item.sortText
+							local t2 = entry2.completion_item.sortText
+							if t1 ~= nil and t2 ~= nil and t1 ~= t2 then
+								return t1 < t2
+							end
+						end,
+						compare.score,
+					},
 				},
 			})
 

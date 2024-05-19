642a1095d267d67b2d158c68a7a20d270720bcbd
Sagi Carmel <ace.code.pt@gmail.com>
2024-05-19 18:37:22 +0300

    feat: made oil refresh on files watch

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,22 +1,22 @@
 return {
-   'stevearc/oil.nvim',
+	"stevearc/oil.nvim",
 
-   opts = {},
-   config = function()
-      require("oil").setup({
+	opts = {},
+	config = function()
+		require("oil").setup({
+			experimental_watch_for_changes = true,
+			prompt_save_on_select_new_entry = false,
+			skip_confirm_for_simple_edits = true,
+			delete_to_trash = true,
+			view_options = {
+				-- Show files and directories that start with "."
+				show_hidden = true,
+			},
+		})
 
-         prompt_save_on_select_new_entry = false,
-         skip_confirm_for_simple_edits = true,
-         delete_to_trash = true,
-         view_options = {
-            -- Show files and directories that start with "."
-            show_hidden = true,
-         }
-      })
+		vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+	end,
 
-      vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-   end,
-
-   -- Optional dependencies
-   dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- Optional dependencies
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 }

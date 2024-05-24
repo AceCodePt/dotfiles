37166917204725469f31fdabcaa8c50072347682
Sagi Carmel <ace.code.pt@gmail.com>
2024-05-24 10:07:35 +0300

    feat: made terminal on create

--- .config/nvim/lua/plugins/terminal.lua
+++ .config/nvim/lua/plugins/terminal.lua
@@ -4,31 +4,29 @@ return {
 		version = "*",
 		config = function()
 			local util = require("util.map")
-			require("toggleterm").setup()
-			function _G.set_terminal_keymaps()
-				local opts = { buffer = 0, noremap = true }
-				util.map("t", "<ESC>", [[<C-\><C-n>]], opts)
-				util.map("t", "<C-t>", function()
-					vim.cmd([[ToggleTerm]])
-				end, opts)
-				util.map("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-				util.map("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-				util.map("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-				util.map("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-				util.map("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
-			end
-
-			-- Set mapping
-			vim.cmd("autocmd! TermOpen term://*toggleterm* lua set_terminal_keymaps()")
-
-			-- Start by inserting by default
-			vim.api.nvim_create_autocmd({ "TermOpen", "BufEnter" }, {
-				pattern = { "term://*toggleterm*" },
-				callback = function()
-					vim.cmd(":startinsert")
+			local Terminal = require("toggleterm.terminal").Terminal
+			require("toggleterm").setup({
+				start_in_insert = true,
+				persist_mode = false,
+				on_create = function(t)
+					local opts = { buffer = t.bufnr, noremap = true }
+					util.map("t", "<ESC>", [[<C-\><C-n>]], opts)
+					util.map("t", "<C-t>", function()
+						vim.cmd([[ToggleTerm]])
+					end, opts)
+					util.map("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+					util.map("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+					util.map("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+					util.map("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
 				end,
 			})
-			util.map("n", "<C-t>", [[<Cmd> ToggleTerm<CR>]])
+
+			util.map("t", "<C-q>", "<CMD>wa<CR><CMD>qa<CR>")
+			-- Note: this is working on alacrity for me
+			util.map({ "n", "t" }, "<C-s-t>", function()
+				Terminal:new():toggle()
+			end)
+			util.map({ "n", "t" }, "<C-t>", [[<Cmd> ToggleTerm<CR>]])
 		end,
 	},
 }

dc751ab4a1b0f41a0601dfad53cb9c3213e9f9f2
Sagi Carmel <ace.code.pt@gmail.com>
2024-05-26 14:07:31 +0300

    chore: cleaning

--- .config/nvim/lua/plugins/autoclose.lua
+++ .config/nvim/lua/plugins/autoclose.lua
@@ -1,26 +1,22 @@
 return {
-   {
-      "m4xshen/autoclose.nvim",
-      opts = {
-         options = {
-            disabled_filetypes = { "text" },
-            disable_when_touch = true,
-            pair_spaces = true,
-         },
-      },
-   },
-   "windwp/nvim-ts-autotag",
-   {
-      "kylechui/nvim-surround",
-      version = "*",
-      event = "VeryLazy",
-      config = function()
-         require("nvim-surround").setup({
-            keymaps = {
-               normal = "gs",
-               normal_cur = "gss",
-            },
-         })
-      end,
-   },
+	{
+		"m4xshen/autoclose.nvim",
+		config = function()
+			require("autoclose").setup()
+		end,
+	},
+	-- "windwp/nvim-ts-autotag",
+	{
+		"kylechui/nvim-surround",
+		version = "*",
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				keymaps = {
+					normal = "gs",
+					normal_cur = "gss",
+				},
+			})
+		end,
+	},
 }

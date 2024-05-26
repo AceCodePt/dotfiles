29f1ce81802d036842f7f596164012b5b5ebf59b
Sagi Carmel <ace.code.pt@gmail.com>
2024-05-26 14:08:03 +0300

    chore: making terminal commands more flexible

--- .config/nvim/lua/plugins/terminal.lua
+++ .config/nvim/lua/plugins/terminal.lua
@@ -5,15 +5,18 @@ return {
 		config = function()
 			local util = require("util.map")
 			local Terminal = require("toggleterm.terminal").Terminal
+			local char = "t"
 			require("toggleterm").setup({
+				open_mapping = "<c-" .. char .. ">",
 				start_in_insert = true,
 				persist_mode = false,
 				on_create = function(t)
 					local opts = { buffer = t.bufnr, noremap = true }
 					util.map("t", "<ESC>", [[<C-\><C-n>]], opts)
-					util.map("t", "<C-t>", function()
-						vim.cmd([[ToggleTerm]])
-					end, opts)
+					-- To close the currently open terminal
+					util.map({ "n", "t" }, "<C-w>", [[<ESC><CMD>q<CR>]], opts)
+
+					-- window movements
 					util.map("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
 					util.map("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
 					util.map("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
@@ -22,11 +25,10 @@ return {
 			})
 
 			util.map("t", "<C-q>", "<CMD>wa<CR><CMD>qa<CR>")
-			-- Note: this is working on alacrity for me
-			util.map({ "n", "t" }, "<C-s-t>", function()
+			-- Note: The use of shift keys didn't work on regular terminal
+			util.map({ "n", "t" }, "<c-s-" .. char .. ">", function()
 				Terminal:new():toggle()
 			end)
-			util.map({ "n", "t" }, "<C-t>", [[<Cmd> ToggleTerm<CR>]])
 		end,
 	},
 }

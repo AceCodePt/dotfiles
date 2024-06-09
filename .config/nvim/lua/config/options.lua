9116620bb9a03d76ffb958dce94cad219d57257a
Sagi Carmel <ace.code.pt@gmail.com>
2024-06-09 11:02:14 +0300

    chore: mouse stuff is now disabled

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,9 +1,8 @@
 -- Set highlight on search
 vim.o.hlsearch = false
 
--- Disable mouse mode
-vim.o.mouse = false
-vim.opt.mousemoveevent = false
+-- Disable mouse stuff
+vim.opt.mouse = ""
 
 -- Make line numbers default
 vim.wo.number = true
@@ -13,7 +12,7 @@ vim.wo.rnu = true
 vim.opt.scrolloff = 10
 
 -- Sync clipboard between OS and Neovim.
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -26,7 +25,7 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = "yes"
 
 vim.o.tabstop = 2
 vim.o.expandtab = true
@@ -37,7 +36,7 @@ vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = "menuone,noselect"
 
 -- Have colors (I'm using ZSH)
 vim.o.termguicolors = true

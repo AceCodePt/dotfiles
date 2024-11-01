d0a176d724496bc3e100ad0b53de6253f20a183b
Sagi Carmel <ace.code.pt@gmail.com>
2024-11-01 12:25:16 +0200

    pack: added auto tag + auto pair

--- .config/nvim/lua/plugins/autoclose.lua
+++ .config/nvim/lua/plugins/autoclose.lua
@@ -1,5 +1,23 @@
 return {
-	"windwp/nvim-ts-autotag",
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-autopairs").setup({
+				check_ts = true,
+
+				disable_filetype = { "TelescopePrompt", "vim" },
+			})
+		end,
+		-- use opts = {} for passing setup options
+		-- this is equivalent to setup({}) function
+	},
 	{
 		"kylechui/nvim-surround",
 		version = "*",

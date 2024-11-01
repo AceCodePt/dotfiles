9e98c5c521dd8202b539e6ad007bebfa706939fb
Sagi Carmel <ace.code.pt@gmail.com>
2024-11-01 12:12:51 +0200

    chore: changed to virtual text

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -90,7 +90,7 @@ return {
 
 			vim.lsp.handlers["textDocument/publishDiagnostics"] =
 				vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-					virtual_text = false,
+					virtual_text = true,
 				})
 		end,
 	},

3f1ab4b6e74b4a9a90cd4419dc7ee53b355c41b8
Sagi Carmel <ace.code.pt@gmail.com>
2024-11-13 17:22:49 +0200

    chore: updated my zsh

--- .config/nvim/update.zsh
+++ .config/nvim/update.zsh
@@ -1,5 +1,4 @@
 sudo curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-mv -f nvim.appimage $HOME/.local/bin/nvim
-sudo chmod u+x $HOME/.local/bin/nvim
-cd ~/dotfiles
-exec stow --adopt .
+chmod u+x nvim.appimage
+mkdir -p /opt/nvim
+mv -f nvim.appimage /opt/nvim

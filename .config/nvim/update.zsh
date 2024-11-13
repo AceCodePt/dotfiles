b433ea7f4f512806e419f840490ea93373a88507
Sagi Carmel <ace.code.pt@gmail.com>
2024-11-13 20:20:45 +0200

    feat: have update for nvim

--- .config/nvim/update.zsh
+++ .config/nvim/update.zsh
@@ -1,4 +1,4 @@
-sudo curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-mkdir -p /opt/nvim
-mv -f nvim.appimage /opt/nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo tar -C /home/sagi/dotfiles/.local/bin -xzf nvim-linux64.tar.gz
+rm -rf nvim-linux64.tar.gz
+mv -f /home/sagi/dotfiles/.local/bin/nvim-linux64 /home/sagi/dotfiles/.local/bin/nvim

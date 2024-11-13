67152a1403cdb59dbb88d482680ad50565dc2267
Sagi Carmel <ace.code.pt@gmail.com>
2024-11-13 21:12:53 +0200

    feat: made <leader>m to be powerful

--- .config/nvim/lua/config/custom-keymaps/ts_ls.lua
+++ .config/nvim/lua/config/custom-keymaps/ts_ls.lua
@@ -15,21 +15,56 @@ function M.init(bufnr)
 	nmap("<leader>rs", ":TermExec cmd='pnpm start'<cr>", "pnpm [R]un [S]tart")
 
 	nmap("<leader>m", function()
-		vim.lsp.buf.code_action({
-			apply = true,
-			filter = function(action)
-				if
-					string.find(action.title, "Add braces to arrow function")
-					or string.find(action.title, "Remove braces from arrow function")
-					or string.find(action.title, "Convert named export to default export")
-					or string.find(action.title, "Convert default export to named export")
-				then
-					return true
-				else
-					return false
+		local params = vim.lsp.util.make_range_params()
+
+		params.context = {
+			triggerKind = vim.lsp.protocol.CodeActionTriggerKind.Invoked,
+			diagnostics = vim.lsp.diagnostic.get_line_diagnostics(),
+		}
+
+		local priorities = {
+			'Add import from "[^"]+"',
+			"Add braces to arrow function",
+			"Remove braces from arrow function",
+			"Convert named export to default export",
+			"Convert default export to named export",
+			"Convert to named function",
+			"import type",
+			"^Use 'type ",
+			"^Replace '[^']+' with '[^']+'$",
+		}
+
+		vim.lsp.buf_request(bufnr, "textDocument/codeAction", params, function(_, results, _, _)
+			local mapped = vim.iter(results)
+				:map(function(item)
+					local priorities_iter = vim.iter(ipairs(priorities))
+					local priority_item = priorities_iter
+						:filter(function(_, priority_text)
+							return string.find(item.title, priority_text)
+						end)
+						:totable()[1]
+
+					local priority = priority_item and (100 + #priorities - priority_item[1]) or 0
+
+					return { text = item.title, priority = priority }
+				end)
+				:totable()
+
+			table.sort(mapped, function(a, b)
+				if a.priority == b.priority then
+					return #a.text < #b.text
 				end
-			end,
-		})
+				return a.priority > b.priority
+			end)
+			-- vim.notify(vim.inspect(mapped))
+
+			vim.lsp.buf.code_action({
+				apply = true,
+				filter = function(action)
+					return action.title == mapped[1].text
+				end,
+			})
+		end)
 	end, "Toggle function")
 end
 

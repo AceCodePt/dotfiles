1b590185c205b3a2391c52e764408a6d77b04020
Sagi Carmel <ace.code.pt@gmail.com>
2025-06-06 10:58:28 +0300

    feat: added mssql support

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -112,7 +112,7 @@ return {
 		"kristijanhusak/vim-dadbod-ui",
 		dependencies = {
 			{ "tpope/vim-dadbod", lazy = true },
-			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql", "jdbc" }, lazy = true }, -- Optional
 		},
 		cmd = {
 			"DBUI",

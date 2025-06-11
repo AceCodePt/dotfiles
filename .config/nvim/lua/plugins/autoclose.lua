6375d19f979dc4b3af241d960a57dd66a5cefb9f
Sagi Carmel <ace.code.pt@gmail.com>
2025-06-11 13:05:16 +0300

    feat: moved auto close to a better one

--- .config/nvim/lua/plugins/autoclose.lua
+++ .config/nvim/lua/plugins/autoclose.lua
@@ -1,28 +1,6 @@
 return {
 	{
 		"windwp/nvim-ts-autotag",
-		config = function()
-			require("nvim-ts-autotag").setup()
-		end,
-	},
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = function()
-			require("nvim-autopairs").setup({
-				check_ts = true,
-
-				disable_filetype = { "TelescopePrompt", "vim" },
-			})
-		end,
-	},
-	{ "tpope/vim-repeat" },
-	{
-		"kylechui/nvim-surround",
-		version = "*",
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup()
-		end,
+		opts = {},
 	},
 }

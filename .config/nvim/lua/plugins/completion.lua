909c6846071df1d181f49f69896b61baa636b95b
Sagi Carmel <ace.code.pt@gmail.com>
2025-06-11 14:00:32 +0300

    chore: cleaning and start on n

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -25,7 +25,6 @@ return {
 			local map = require("util.map").map
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
-			-- local types = require("cmp.types")
 			local compare = require("cmp.config.compare")
 
 			vim.keymap.set({ "i", "s" }, "<C-l>", function()
@@ -41,18 +40,6 @@ return {
 					{ name = "buffer" },
 				},
 			})
-
-			-- local modified_priority = {
-			-- 	[types.lsp.CompletionItemKind.Variable] = 1,
-			-- 	[types.lsp.CompletionItemKind.Method] = 2,
-			-- 	[types.lsp.CompletionItemKind.Snippet] = 0, -- top
-			-- 	[types.lsp.CompletionItemKind.Keyword] = 3,
-			-- 	[types.lsp.CompletionItemKind.Text] = 100, -- bottom
-			-- }
-
-			-- local function modified_kind(kind)
-			-- 	return modified_priority[kind] or kind
-			-- end
 
 			cmp.setup({
 				preselect = cmp.PreselectMode.Item,
@@ -71,7 +58,17 @@ return {
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+					["<C-j>"] = cmp.mapping({
+						i = function(fallback)
+							if cmp.visible() then
+								cmp.mapping.select_next_item()(fallback)
+							else
+								cmp.mapping.complete()(fallback)
+							end
+						end,
+						c = cmp.mapping.select_next_item(),
+						n = cmp.mapping.complete(),
+					}),
 					["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
 					["<CR>"] = cmp.mapping(
 						cmp.mapping.confirm({
@@ -80,7 +77,6 @@ return {
 						}),
 						{ "i" }
 					),
-					["<C-Space>"] = cmp.mapping.complete({}),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp", priority = 8 },

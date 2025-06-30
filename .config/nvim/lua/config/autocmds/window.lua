91084cc06ff04c85e94afde113a23a663ba0c993
Sagi Carmel <ace.code.pt@gmail.com>
2025-06-30 17:11:31 +0300

    chore: added window resize

---
+++ .config/nvim/lua/config/autocmds/window.lua
@@ -0,0 +1,10 @@
+
+local augroup = vim.api.nvim_create_augroup("UserWindow", { clear = true })
+
+-- Auto-resize splits when window is resized
+vim.api.nvim_create_autocmd("VimResized", {
+  group = augroup,
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
+})

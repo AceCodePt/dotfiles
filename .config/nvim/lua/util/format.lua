7f2f7dea914dafba11483b334a61c1ea06f0af6f
Sagi Carmel <ace.code.pt@gmail.com>
2025-07-03 21:19:00 +0300

    chore: allowed for selected text parsing

--- .config/nvim/lua/util/format.lua
+++ .config/nvim/lua/util/format.lua
@@ -4,4 +4,19 @@ function M.format()
   conform.format({ async = true, lsp_format = "fallback" })
 end
 
+---@param text string
+function M.format_text(text)
+  local conform = require("conform")
+  local stuff = conform.list_formatters_for_buffer(0)
+  local lines_to_format = vim.split(text, "\n")
+  local err, new_lines = conform.format_lines(stuff, lines_to_format, { async = false })
+  vim.schedule(function()
+    vim.notify(vim.inspect(err) .. vim.inspect(new_lines))
+  end)
+  if err or not new_lines then
+    return nil
+  end
+  return table.concat(new_lines, "\n")
+end
+
 return M

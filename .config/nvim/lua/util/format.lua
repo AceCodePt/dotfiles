482061bba0f53b8921296fd3c2a4e92326f061d8
Sagi Carmel <ace.code.pt@gmail.com>
2025-07-04 18:59:21 +0300

    chore: format is now first

--- .config/nvim/lua/util/format.lua
+++ .config/nvim/lua/util/format.lua
@@ -1,7 +1,7 @@
 local M = {}
 function M.format()
   local conform = require("conform")
-  conform.format({ async = true, lsp_format = "fallback" })
+  conform.format({ async = true, lsp_format = "first" })
 end
 
 ---@param text string

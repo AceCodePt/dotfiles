eb92fad5a3d52b33fbd01858c17b31f9f95998c9
Sagi Carmel <ace.code.pt@gmail.com>
2025-07-06 22:45:20 +0300

    feat: created copy under diagnostic

--- .config/nvim/lua/config/autocmds/lsp.lua
+++ .config/nvim/lua/config/autocmds/lsp.lua
@@ -1,31 +1,59 @@
+local map = require("util.map").map
+
+
+local function copyDiagnosticUnderCursor(buf)
+  local diagnostics = vim.diagnostic.get(buf, {
+    lnum = vim.fn.line('.') - 1,
+    col = vim.fn.col('.') - 1,
+  })
+  -- Check if any diagnostics were found
+  if #diagnostics == 0 then
+    print("No diagnostic under cursor")
+    return
+  end
+
+  -- Format all diagnostic messages into a numbered list
+  local messages = {}
+  for i, d in ipairs(diagnostics) do
+    table.insert(messages, string.format("%d. %s", i, d.message))
+  end
+  local formatted_message = table.concat(messages, "\n")
+
+  -- Copy the formatted string to the system clipboard
+  vim.fn.setreg('+', formatted_message)
+  print("Copied all diagnostics to clipboard!")
+end
+
 -- LSP keymaps
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(event)
     local opts = { buffer = event.buf }
 
     -- Navigation
-    vim.keymap.set('n', 'gD', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'gs', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    map('n', 'gd', vim.lsp.buf.definition, opts)
+    map('n', 'gs', vim.lsp.buf.declaration, opts)
+    map('n', 'gr', vim.lsp.buf.references, opts)
+    map('n', 'gi', vim.lsp.buf.implementation, opts)
 
     -- Information
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    map('n', 'K', vim.lsp.buf.hover, opts)
 
     -- Code actions
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    map('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    map('n', '<leader>rn', vim.lsp.buf.rename, opts)
 
     -- Diagnostics
-    vim.keymap.set('n', '<leader>nd', function()
+    map('n', 'gn', function()
       vim.diagnostic.jump({ count = 1, float = true })
     end
     , opts)
-    vim.keymap.set('n', '<leader>pd', function()
+    map('n', 'gp', function()
       vim.diagnostic.jump({ count = -1, float = true })
     end
     , opts)
-    vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+    map('n', '<leader>q', vim.diagnostic.setloclist, opts)
+    map({ 'n', 'v' }, '<leader>yd', function()
+      copyDiagnosticUnderCursor(event.buf)
+    end, opts)
   end,
 })

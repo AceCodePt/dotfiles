08f3f155f9d434d7fe210cf72fd37edc74629df7
Sagi Carmel <ace.code.pt@gmail.com>
2025-07-07 22:07:53 +0300

    chore: my weird autocmd

--- .config/nvim/lua/config/autocmds/custom.lua
+++
@@ -1,8 +0,0 @@
-local convert_selection_to_camel = require("util.camel_case").convert_selection_to_camel
-
--- Create a user command that can be called on a range.
--- Usage: Select text, then type: :CamelCase
-vim.api.nvim_create_user_command("CamelCase", convert_selection_to_camel, {
-  range = true,
-  desc = "Convert selected text to camelCase",
-})

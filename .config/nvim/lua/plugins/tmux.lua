4716944dfc8a5966bcaf4d58f4cecff26a62de6c
Sagi Carmel <ace.code.pt@gmail.com>
2025-07-15 11:15:35 +0300

    chore: annoying keybind of tmux

--- .config/nvim/lua/plugins/tmux.lua
+++ .config/nvim/lua/plugins/tmux.lua
@@ -1,10 +1,19 @@
 return {
   {
     "aserowy/tmux.nvim",
-    opts = {
-      copy_sync = {
-        enable = false
-      },
-    }
+    opts =
+        function()
+          local tmux = require("tmux")
+          tmux.setup(
+            {
+              copy_sync = {
+                enable = false
+              },
+              resize = {
+                enable_default_keybindings = false,
+              },
+            }
+          )
+        end
   },
 }

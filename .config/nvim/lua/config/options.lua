d2ebe941785163a6367e7b3d67193b9e0329b859
Sagi Carmel <ace.code.pt@gmail.com>
2025-07-16 08:45:16 +0300

    chore: bye bye line number

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,8 +4,8 @@ vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
 
 -- Basic settings
-vim.opt.number = true         -- Line numbers
-vim.opt.relativenumber = true -- Relative line numbers
+vim.opt.number = false         -- Line numbers
+vim.opt.relativenumber = false -- Relative line numbers
 vim.opt.cursorline = true     -- Highlight current line
 vim.opt.wrap = true           -- Don't wrap lines
 vim.opt.scrolloff = 10        -- Keep 10 lines above/below cursor

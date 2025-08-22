1d0251912b243c19df87e58898a134cc4c7dfca0
Sagi Carmel <ace.code.pt@gmail.com>
2025-08-22 15:05:44 +0300

    feat: now tokyonight

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,2 +1,10 @@
-vim.pack.add({ "https://github.com/navarasu/onedark.nvim.git" })
-vim.cmd.colorscheme [[onedark]]
+vim.pack.add({ "https://github.com/folke/tokyonight.nvim.git" })
+vim.cmd.colorscheme [[tokyonight]]
+
+-- Get the background color from the 'Normal' highlight group
+local normal_bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg
+
+-- Apply it to TabLineSel. This keeps the foreground but fixes the background.
+vim.api.nvim_set_hl(0, "TabLineSel", {
+  bg = normal_bg,
+})

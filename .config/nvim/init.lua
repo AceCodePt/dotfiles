61b7ddcdb1e5e8a13f10d052ed6e6352354a2130
Sagi Carmel <ace.code.pt@gmail.com>
2025-08-22 09:06:04 +0300

    feat: trying nvim nightly

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,7 @@
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
-require("config.lazy")
-require("config.general")
-require("config.overrides")
 require("config.session")
 require("config.tabs")
-require("config.log").setup(vim.log.levels.INFO)
+
+require("plugins.theme")
--- .config/nvim/update.zsh
+++ .config/nvim/update.zsh
@@ -1,4 +1,4 @@
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.appimage
 sudo tar -C /home/sagi/dotfiles/.local/bin -xzf nvim-linux64.tar.gz
 rm -rf nvim-linux64.tar.gz
 mv -f /home/sagi/dotfiles/.local/bin/nvim-linux64 /home/sagi/dotfiles/.local/bin/nvim

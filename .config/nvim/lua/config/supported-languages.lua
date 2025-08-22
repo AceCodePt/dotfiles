a9e22f6559290d30c5022093a8c551bfd4d4f0c9
Sagi Carmel <ace.code.pt@gmail.com>
2025-08-22 12:39:00 +0300

    feat: added language name

--- .config/nvim/lua/config/supported-languages.lua
+++ .config/nvim/lua/config/supported-languages.lua
@@ -1,19 +1,35 @@
 local M = {
-  { ts = "lua",    fts = "lua", lsp = "lua_ls" },
-  { ts = "python", fts = "py",  lsp = "pyright" },
+  { language = "lua",    lsp = { name = "lua_ls" },        ts = "lua",    fts = "lua" },
+  { language = "python", lsp = { name = "pyright" },       ts = "python", fts = "py" },
+  { language = "astro",  lsp = { name = "astro" },         ts = "astro",  fts = "astro" },
+  { language = "css",    lsp = { name = "cssls" },         ts = "css",    fts = { "css", "scss", "less" } },
+  { language = "html",   lsp = { name = "html" },          ts = "html",   fts = "html" },
+  { language = "go",     lsp = { name = "gopls" },         ts = "go",     fts = "go" },
+  { language = "rust",   lsp = { name = "rust_analyzer" }, ts = "rust",   fts = "rs" },
   {
+    language = "javascript",
+    lsp = { name = "ts_ls" },
+    ts = "javascript",
+    fts = { "js", "jsx" }
+  },
+  {
+    language = "typescript",
+    lsp = { name = "ts_ls" },
     ts = {
-      "javascript",
       "typescript",
       "tsx"
     },
-    fts = { "ts", "js", "tsx", "jsx" },
-    lsp = "ts_ls"
+    fts = { "ts", "tsx" }
   },
-  { ts = "astro", fts = "astro",                   lsp = "astro" },
-  { ts = "css",   fts = { "css", "scss", "less" }, lsp = "cssls" },
-  { ts = "go",    fts = "go",                      lsp = "gopls" },
-  { ts = "rust",  fts = "rs",                      lsp = "rust_analyzer" },
+  { language = "json",     lsp = { name = "jsonls" }, ts = "json",     fts = "json" },
+  { language = "yaml",     lsp = { name = "yamlls" }, ts = "yaml",     fts = { "yaml", "yml" } },
+  { language = "markdown", lsp = {},                  ts = "markdown", fts = "md" },
+  {
+    language = "docker",
+    lsp = { name = "docker_language_server" },
+    ts = "dockerfile",
+    fts = { "Dockerfile" },
+  },
 }
 
 return M

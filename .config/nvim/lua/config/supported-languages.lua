016940f8159657e004a7a95fb3b48ca93412b9e6
Sagi Carmel <ace.code.pt@gmail.com>
2025-08-22 15:05:15 +0300

    feat: extending the lsp

--- .config/nvim/lua/config/supported-languages.lua
+++ .config/nvim/lua/config/supported-languages.lua
@@ -1,34 +1,152 @@
 local M = {
-  { language = "lua",    lsp = { name = "lua_ls" },        ts = "lua",    fts = "lua" },
-  { language = "python", lsp = { name = "pyright" },       ts = "python", fts = "py" },
-  { language = "astro",  lsp = { name = "astro" },         ts = "astro",  fts = "astro" },
-  { language = "css",    lsp = { name = "cssls" },         ts = "css",    fts = { "css", "scss", "less" } },
-  { language = "html",   lsp = { name = "html" },          ts = "html",   fts = "html" },
-  { language = "go",     lsp = { name = "gopls" },         ts = "go",     fts = "go" },
-  { language = "rust",   lsp = { name = "rust_analyzer" }, ts = "rust",   fts = "rs" },
   {
-    language = "javascript",
-    lsp = { name = "ts_ls" },
-    ts = "javascript",
-    fts = { "js", "jsx" }
+    fts = "lua",
+    lsp = { name = "lua_ls" },
+    ts = "lua",
+    formatters = {}
   },
   {
-    language = "typescript",
+    fts = "python",
+    ts = "python",
+    formatters = { "ruff" },
+    lsp = {
+      name = "pyright",
+      config = {
+        filetypes = { "python" },
+        settings = {
+          python = {
+            pythonPath = (function()
+              local venv = vim.fn.findfile(".venv/bin/python", vim.fn.getcwd() .. ";")
+              if venv ~= "" then
+                return venv
+              end
+              return vim.fn.exepath("python3")
+            end)(),
+          },
+        },
+      }
+    },
+  },
+  {
+    fts = "astro",
+    ts = "astro",
+    formatters = { "prettierd", "prettier" },
+    lsp = {
+      name = "astro",
+      config = {
+        on_attach = function(client, _)
+          vim.api.nvim_create_autocmd("BufWritePost", {
+            pattern = { "*.js", "*.ts" },
+            group = vim.api.nvim_create_augroup("astro_ondidchangetsorjsfile", { clear = true }),
+            callback = function(ctx)
+              client.notify("workspace/didChangeWatchedFiles", {
+                changes = {
+                  {
+                    uri = ctx.match,
+                    type = 2, -- 1 = Created, 2 = Changed, 3 = Deleted
+                  },
+                },
+              })
+            end,
+          })
+        end,
+      }
+    },
+  },
+  {
+    fts = "css",
+    lsp = { name = "cssls" },
+    ts = "css",
+    formatters = {}
+  },
+  {
+    fts = "html",
+    lsp = { name = "html" },
+    ts = "html",
+    formatters = {}
+  },
+  {
+    fts = "go",
+    lsp = { name = "gopls" },
+    ts = "go",
+    formatters = {}
+  },
+  {
+    fts = "rust",
+    lsp = { name = "rust_analyzer" },
+    ts = "rust",
+    formatters = { "rustfmt" }
+  },
+  {
+    fts = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
     lsp = { name = "ts_ls" },
     ts = {
+      "javascript",
       "typescript",
       "tsx"
     },
-    fts = { "ts", "tsx" }
+    formatters = { "prettierd", "prettier" }
   },
-  { language = "json",     lsp = { name = "jsonls" }, ts = "json",     fts = "json" },
-  { language = "yaml",     lsp = { name = "yamlls" }, ts = "yaml",     fts = { "yaml", "yml" } },
-  { language = "markdown", lsp = {},                  ts = "markdown", fts = "md" },
   {
-    language = "docker",
+    fts = "json",
+    lsp = { name = "jsonls" },
+    ts = "json",
+    formatters = {},
+    config = {
+      settings = {
+        json = {
+          -- Schemas https://www.schemastore.org
+          schemas = {
+            {
+              fileMatch = { "package.json" },
+              url = "https://json.schemastore.org/package.json",
+            },
+            {
+              fileMatch = { "tsconfig*.json" },
+              url = "https://json.schemastore.org/tsconfig.json",
+            },
+            {
+              fileMatch = {
+                ".prettierrc",
+                ".prettierrc.json",
+                "prettier.config.json",
+              },
+              url = "https://json.schemastore.org/prettierrc.json",
+            },
+            {
+              fileMatch = { ".eslintrc", ".eslintrc.json" },
+              url = "https://json.schemastore.org/eslintrc.json",
+            },
+            {
+              fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
+              url = "https://json.schemastore.org/babelrc.json",
+            },
+            {
+              fileMatch = { "now.json", "vercel.json" },
+              url = "https://json.schemastore.org/now.json",
+            },
+          },
+        },
+      },
+    }
+  },
+  {
+    fts = "yaml",
+    lsp = { name = "yamlls" },
+    ts = "yaml",
+    formatters = {}
+  },
+  {
+    fts = "markdown",
+    lsp = { name = "markdown_oxide" },
+    ts = "markdown",
+    formatters = {}
+  },
+  {
+    fts = "dockerfile",
     lsp = { name = "docker_language_server" },
     ts = "dockerfile",
-    fts = { "Dockerfile" },
+    formatters = {}
   },
 }
 
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,17 +1,26 @@
 vim.pack.add({
-  { src = "https://github.com/mason-org/mason.nvim" },
   { src = 'https://github.com/neovim/nvim-lspconfig' },
-  { src = 'https://github.com/mason-org/mason-lspconfig.nvim' }
+  { src = "https://github.com/mason-org/mason.nvim" },
 })
 
 local supported_languages = require("config.supported-languages")
-local ensure_installed_languages = vim.iter(supported_languages)
-    :map(function(lang)
-      return lang.lsp.name
-    end)
-    :totable()
 
 
-require("mason-lspconfig").setup({
-  ensure_installed = ensure_installed_languages
-})
+require("mason").setup({})
+for _, lang in pairs(supported_languages) do
+  local opts = {}
+
+  -- Merge custom config if it exists
+  if lang.lsp.config then
+    opts = vim.tbl_deep_extend("force", opts, lang.lsp.config)
+    if lang.lsp.config.on_attach then
+      opts.on_attach = function(client, bufnr)
+        lang.lsp.config.on_attach(client, bufnr)
+      end
+    end
+  end
+
+  -- Set up the LSP server
+  vim.lsp.config(lang.lsp.name, opts)
+  vim.lsp.enable(lang.lsp.name)
+end

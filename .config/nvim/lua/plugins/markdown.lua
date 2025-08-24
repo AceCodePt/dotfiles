3a990c57829b5abf62191fc45ec18dc8d8a0036c
Sagi Carmel <ace.code.pt@gmail.com>
2025-08-24 16:47:50 +0300

    feat: added support for markdown-preview

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,9 +1,18 @@
-return {
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-  }
+vim.api.nvim_create_autocmd('PackChanged', {
+  group = vim.api.nvim_create_augroup('MarkdownPreviewBuild', { clear = true }),
+  pattern = 'markdown-preview.nvim',
+  callback = function(args)
+    local plugin_path = args.data.path
+    vim.system({ 'yarn', 'install' }, {
+      cwd = plugin_path,
+      stdout = true,
+      stderr = true,
+    })
+  end
+})
 
-}
+vim.pack.add({ { src = "https://github.com/iamcco/markdown-preview.nvim" } })
+
+
+vim.g.mkdp_filetypes = { "markdown" }
+vim.g.mkdp_echo_preview_url = 1

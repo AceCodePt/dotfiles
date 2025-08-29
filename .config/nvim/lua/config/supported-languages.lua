0b0a5fb71ce16b8c041399770a860385801e1af1
Sagi Carmel <ace.code.pt@gmail.com>
2025-08-29 16:19:30 +0300

    feat: lua now with types

--- .config/nvim/lua/config/supported-languages.lua
+++ .config/nvim/lua/config/supported-languages.lua
@@ -1,7 +1,23 @@
 local M = {
   {
     fts = "lua",
-    lsp = { name = "lua_ls" },
+    lsp = {
+      name = "lua_ls",
+      config = {
+        settings = {
+          Lua = {
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = { 'vim' },
+            },
+            workspace = {
+              -- Make the server aware of Neovim runtime files
+              library = vim.api.nvim_get_runtime_file('', true),
+            },
+          },
+        },
+      }
+    },
     treesitter = "lua",
     formatters = {}
   },

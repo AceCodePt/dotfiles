2fc470f289d0b6d47469aeec81c85550c5c6efdd
Sagi Carmel <ace.code.pt@gmail.com>
2025-08-31 09:49:38 +0300

    feat: added comment + mini.ai

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,10 +1,36 @@
 vim.pack.add({
   { src = "https://github.com/stevearc/conform.nvim" },
-  { src = "https://github.com/numToStr/Comment.nvim" }
+  { src = "https://github.com/numToStr/Comment.nvim" },
+  { src = "https://github.com/echasnovski/mini.ai" }
 })
 
 require('Comment').setup({
+  -- ignore new line
   ignore = '^$'
+})
+
+local miniAi = require("mini.ai")
+miniAi.setup({
+  -- Table with textobject id as fields, textobject specification as values.
+  -- Also use this to disable builtin textobjects. See |MiniAi.config|.
+  custom_textobjects = {
+    f = miniAi.gen_spec.treesitter({ i = "@function.inner", a = "@function.outer" }),
+    -- s = ai.gen_spec.treesitter({ i = "@function.signature.inner", a = "function.signature.outer" }),
+    c = miniAi.gen_spec.treesitter({ i = "@class.inner", a = "@class.outer" }),
+  },
+
+  -- Number of lines within which textobject is searched
+  n_lines = 50,
+
+  -- How to search for object (first inside current line, then inside
+  -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+  -- 'cover_or_nearest', 'next', 'previous', 'nearest'.
+  search_method = "cover_or_nearest",
+
+  -- Whether to disable showing non-error feedback
+  -- This also affects (purely informational) helper messages shown after
+  -- idle time if user input is required.
+  silent = false,
 })
 
 local supported_languages = require("config.supported-languages")

51a61617bcd7a6158cbe93145c6a9b8b76f56141
Sagi Carmel <ace.code.pt@gmail.com>
2025-08-31 09:45:38 +0300

    feat: added ensure installed

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,9 +6,37 @@ vim.pack.add({
 local supported_languages = require("config.supported-languages")
 
 require("mason").setup({})
+local _ = require "mason-core.functional"
+local registry = require "mason-registry"
 
+local cached_specs = _.lazy(registry.get_all_package_specs)
+registry:on("update:success", function()
+  cached_specs = _.lazy(registry.get_all_package_specs)
+end)
+
+local function get_lspconfig_to_package()
+  ---@type table<string, string>
+  local lspconfig_to_package = {}
+  for _, pkg_spec in ipairs(cached_specs()) do
+    local lspconfig = vim.tbl_get(pkg_spec, "neovim", "lspconfig")
+    if lspconfig then
+      lspconfig_to_package[lspconfig] = pkg_spec.name
+    end
+  end
+
+  return lspconfig_to_package
+end
+
+local lspconfig_to_package = get_lspconfig_to_package()
 for _, lang in pairs(supported_languages) do
   local opts = {}
+  local pkg_name = lspconfig_to_package[lang.lsp.name]
+  local pkg = registry.get_package(pkg_name)
+
+  -- ensure installed
+  if pkg and not pkg:is_installed() then
+    pkg:install()
+  end
 
   -- Merge custom config if it exists
   if lang.lsp.config then

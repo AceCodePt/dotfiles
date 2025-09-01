8a3b467d969a75a982ca58a05a8d7556d14e0c7c
Sagi Carmel <ace.code.pt@gmail.com>
2025-09-01 10:08:32 +0300

    feat: code actions are now fzf+tmux

--- .config/nvim/lua/config/autocmds/lsp.lua
+++ .config/nvim/lua/config/autocmds/lsp.lua
@@ -1,4 +1,5 @@
 local map = require("util.map").map
+local tmux_popup = require("util.fzf_tmux").tmux_popup
 
 
 local function copyDiagnosticUnderCursor(buf)
@@ -26,13 +27,15 @@ end
 
 -- LSP keymaps
 vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(event)
-    local opts = { buffer = event.buf }
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    local bufnr = args.buf
+    local opts = { buffer = bufnr }
 
     -- Unset 'formatexpr'
-    vim.bo[event.buf].formatexpr = nil
+    vim.bo[bufnr].formatexpr = nil
     -- Unset 'omnifunc'
-    vim.bo[event.buf].omnifunc = nil
+    vim.bo[bufnr].omnifunc = nil
 
     -- Navigation
     map('n', 'gd', vim.lsp.buf.definition, opts)
@@ -44,7 +47,56 @@ vim.api.nvim_create_autocmd('LspAttach', {
     map('n', 'K', vim.lsp.buf.hover, opts)
 
     -- Code actions
-    map('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    map('n', '<leader>ca',
+      function()
+        ---@type any
+        local params = vim.lsp.util.make_range_params(0, "utf-8")
+        params.context = {
+          triggerKind = vim.lsp.protocol.CodeActionTriggerKind.Invoked,
+          diagnostics = vim.diagnostic.get(bufnr)
+        }
+        local code_action_result = client:request_sync('textDocument/codeAction', params, 2000, bufnr)
+
+        if not code_action_result then
+          return
+        end
+
+        local err = code_action_result.err
+        local results = code_action_result.result
+
+        if err then
+          vim.notify("Error fetching code actions: " .. err.message, vim.log.levels.ERROR)
+          return
+        end
+
+        if results == nil or vim.tbl_isempty(results) then
+          vim.notify("No code actions available.", vim.log.levels.INFO)
+          return
+        end
+        results = vim.iter(results)
+            :filter(function(item)
+              return not item.disabled
+            end)
+            :totable()
+        local actions = vim.iter(ipairs(results))
+            :map(function(index, item)
+              return index .. ") " .. item.title
+            end)
+            :totable()
+
+        local selected_item = tmux_popup(actions, { fzf = true, prompt = "Code Action > " })
+
+        if selected_item == "" then
+          return
+        end
+
+        local index = tonumber(string.match(selected_item, "^%d+"))
+        local selected_action = results[index].command
+        client:exec_cmd(selected_action, { bufnr = bufnr })
+      end
+      , opts)
+
+    -- rename
     map('n', '<leader>rn', vim.lsp.buf.rename, opts)
 
     -- Diagnostics
@@ -58,7 +110,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     , opts)
     map('n', '<leader>q', vim.diagnostic.setloclist, opts)
     map({ 'n', 'v' }, 'yd', function()
-      copyDiagnosticUnderCursor(event.buf)
+      copyDiagnosticUnderCursor(args.buf)
     end, opts)
     map({ 'n', 'v' }, '<leader>f', function()
       local ok, conform = pcall(require, "conform")

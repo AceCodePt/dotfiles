2692988d7fa643cf2408ebb6ff60253162a1e548
Sagi Carmel <ace.code.pt@gmail.com>
2025-09-02 16:57:56 +0300

    chore: async install

--- .config/nvim/lua/plugins/config/treesitter.lua
+++ .config/nvim/lua/plugins/config/treesitter.lua
@@ -5,7 +5,7 @@ local ensure_installed_languages = supported_languages.get_treesitters()
 require('nvim-treesitter.configs').setup({
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
   ensure_installed = ensure_installed_languages,
-  sync_install = true,
+  sync_install = false,
   auto_install = false,
   ignore_install = {},
   modules = {},
@@ -13,4 +13,7 @@ require('nvim-treesitter.configs').setup({
     enable = true,
     additional_vim_regex_highlighting = false,
   },
+  indent = {
+    enable = true
+  }
 })

c62ad05f90c525ba0662e295346c473d645f2803
Sagi Carmel <ace.code.pt@gmail.com>
2025-09-04 09:32:48 +0300

    feat: work on termux!

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,7 +1,8 @@
 return {
   -- cd ~/.local/share/nvim/site/pack/core/opt/blink.cmp && cargo build --release
   url = "https://github.com/Saghen/blink.cmp",
-  build = 'cargo build --release',
+  -- To make it work everywhere (especially termux)
+  build = 'RUSTC_BOOTSTRAP=1 cargo build --release',
   dependencies = {
     { url = "https://github.com/L3MON4D3/LuaSnip",  tag = "v2.*" },
     { url = "https://github.com/folke/lazydev.nvim" },

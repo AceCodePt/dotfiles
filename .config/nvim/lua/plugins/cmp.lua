02d64c98f828b90239b691d64e19455046eb6cd7
Sagi Carmel <ace.code.pt@gmail.com>
2025-10-16 15:57:22 +0300

    feat: moved to tag 2.4

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -4,7 +4,7 @@ return {
   -- To make it work everywhere (especially termux)
   build = 'RUSTC_BOOTSTRAP=1 cargo build --release',
   dependencies = {
-    { url = "https://github.com/L3MON4D3/LuaSnip",  tag = "v2.*" },
+    { url = "https://github.com/L3MON4D3/LuaSnip",  tag = "v2.4.0" },
     { url = "https://github.com/folke/lazydev.nvim" },
     { url = "https://github.com/chrisgrieser/nvim-scissors" }
   }

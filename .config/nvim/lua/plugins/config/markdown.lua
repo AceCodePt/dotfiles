d1f4b67cb3779a0e1898974922aed386fbfbcfbf
Sagi Carmel <sagica5273@gmail.com>
2025-12-31 16:01:58 +0200

    chore: dynamic markdown

--- .config/nvim/lua/plugins/config/markdown.lua
+++ .config/nvim/lua/plugins/config/markdown.lua
@@ -1,15 +1,5 @@
--- vim.api.nvim_create_autocmd('PackChanged', {
---   group = vim.api.nvim_create_augroup('MarkdownPreviewBuild', { clear = true }),
---   pattern = 'markdown-preview.nvim',
---   callback = function(args)
---     local plugin_path = args.data.path
---     vim.system({ 'yarn', 'install' }, {
---       cwd = plugin_path,
---       stdout = true,
---       stderr = true,
---     })
---   end
--- })
+require('render-markdown').setup({
+  completions = { lsp = { enabled = true } },
+})
 
-vim.g.mkdp_filetypes = { "markdown" }
-vim.g.mkdp_echo_preview_url = 1
+require('render-markdown').enable()

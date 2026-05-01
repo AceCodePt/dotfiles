0e62014908e98164f0cf0594587e5f67e12bba7c
Sagi Carmel <sagica5273@gmail.com>
2026-05-01 14:45:49 +0300

    chore:  cmp v.1

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -3,8 +3,10 @@ return {
   url = "https://github.com/Saghen/blink.cmp",
   -- To make it work everywhere (especially termux)
   build = 'RUSTC_BOOTSTRAP=1 cargo build --release',
+
+  version = '1.*',
   dependencies = {
-    { url = "https://github.com/L3MON4D3/LuaSnip",  tag = "v2.4.0" },
+    { url = "https://github.com/L3MON4D3/LuaSnip",          tag = "v2.4.0" },
     { url = "https://github.com/folke/lazydev.nvim" },
     { url = "https://github.com/chrisgrieser/nvim-scissors" }
   }
